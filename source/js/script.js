f8032cbcf8c50a6507ab3820e8e5896d65a4c424
Denis Vladimirov <denis@artkerr.com>
2020-09-02 16:31:10 +0300

    js для модального оrна index.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
+var button = document.querySelector('.best-product__button');
+var popup = document.querySelector('.modal');
+
+
 navMain.classList.remove('main-nav--nojs');
 
 navMain.addEventListener('click', function() {
@@ -10,5 +14,20 @@ navMain.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+
+button.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('modal--show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains('modal--show')) {
+      popup.classList.remove('modal--show');
+    }
   }
 });
