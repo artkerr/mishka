16ddf0a210b256cd459e76a9aad4b34d21ec1327
Denis Vladimirov <denis@artkerr.com>
2020-09-06 11:49:37 +0300

    Звурытие модального окна по клику

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,6 @@ var navToggle = document.querySelector('.main-nav__toggle');
 var popupLinks = document.querySelectorAll('.modal__open');
 var popup = document.querySelector('.modal');
 
-
 // Mobile menu
 
 navMain.classList.remove('main-nav--nojs');
@@ -31,7 +30,15 @@ if (popupLinks.length > 0) {
   }
 };
 
-window.addEventListener('keydown', function (evt) {
+popup.addEventListener ('click', function (evt) {
+  if (!evt.target.closest('.modal__window')) {
+    if (popup.classList.contains('modal--show')) {
+      popup.classList.remove('modal--show');
+      };
+  };
+});
+
+document.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains('modal--show')) {
